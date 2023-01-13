331779b52ac3c64a17ce10e0b23f2a0919d83b10
Gabe <gtorion97@gmail.com>
2023-01-13 10:20:12 -0600

    starting new backend

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User
-from django.contrib import admin
-
-# class Admin(admin.ModelAdmin):
-    # readonly_fields = ('id')
-
-admin.site.register((User))
