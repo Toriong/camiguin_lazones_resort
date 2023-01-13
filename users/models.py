864f47ba695e9b04c990506268b5baac3bcb9abd
Gabe <gtorion97@gmail.com>
2023-01-13 14:09:37 -0600

    creating custom model again

--- users/models.py
+++ users/models.py
@@ -8,12 +8,17 @@ from django.contrib.auth.models import User
     # pass
 
 
+
+
 class Member(models.Model):
-    # email = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    email = models.ForeignKey(User, on_delete=models.CASCADE) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(default=None, null=True)
     from_city = models.CharField(max_length=100, default="")
     from_country = models.CharField(max_length=100, default="")
     referral_count_num = models.IntegerField(default=0)
-    isMale = models.BooleanField(default=True)
+    is_male = models.BooleanField(default=True)
     phone_num = models.CharField(max_length=50)
+
+    @classmethod
+    def create(cls, birth_date, from_city, from_country, referral_count_num, is_male, phone_num):
+        return cls(birth_date=birth_date,from_city=from_city,from_country=from_country, referral_count_num=referral_count_num, is_male=is_male, phone_num=phone_num)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 # Create your views here.
 def testConnection(request):
@@ -10,7 +11,18 @@ def testConnection(request):
 def createUser(request):
     print("request: ", request)
 
-    
+    if request.POST and (request.POST.get('name') == 'createUser'):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        firstName = request.POST.get('firstName')
+        lastName = request.POST.get('lastName')
+
+        if email and password and firstName and lastName:
+            user = User.objects.create_user(None, email, password)
+            return HttpResponse("User was created successfully.")
+        else:
+            return HttpResponse("Did not receive an email, password, first name, or a last name.", status=404)
+        
 
 
     return HttpResponse("Request does not have a name.")
