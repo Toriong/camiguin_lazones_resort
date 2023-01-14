0f209e74f43efc75aab35c25ccf20f404167acca
Gabe <gtorion97@gmail.com>
2023-01-14 15:15:27 -0600

    refactoring get_token function

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import testConnection, createUser, signIn
+from .views import testConnection, createUser, signIn, MyTokenObtainPairView, getAccountInfo
+from rest_framework_simplejwt.views import (TokenRefreshView)
 
 
 
 
 urlpatterns = [
     path('', testConnection, name='connectionCheck'),
+    path('get-token', MyTokenObtainPairView.as_view(), name='sign-in'),
+    path('refresh-token', TokenRefreshView.as_view(), name='refresh-token'),
     path('create-user', createUser, name='create-container'),
     path('sign-in', signIn, name='sign-in'),
+    path('get-account-info', getAccountInfo, name='get-account-info'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,18 +4,37 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate, login
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import User
+import requests
+
+serverDomain = "http://localhost:8000"
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        print("password: ", user)
+        
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['email'] = user.email
+        # ...
+
+        return token
 
 
-def generateToken(user):
-    refresh = RefreshToken.for_user(user)
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
-    return {'refresh': str(refresh), 'access': str(refresh.access_token)}
 
+def getJwtResponse(serverUrl, email, password):
+    return requests.post(url=serverUrl, data={ 'email': email, 'password': password })
 
 # Views below.
-
-
 def testConnection(request):
     return HttpResponse("Sever is live on PORT: 8000")
 
@@ -29,7 +48,9 @@ def signIn(request):
 
         if user is not None:
             targetUser = User.objects.get(email=email)
-            user = { "token": generateToken(user), 'firstName': targetUser.first_name, 'lastName': targetUser.last_name }
+            serverUrl = "{serverDomain}/users/get-token".format(serverDomain=serverDomain)
+            response = requests.post(url=serverUrl, data={ 'email': email, 'password': request.GET.get('password') })
+            user = { "token": response.json(), 'firstName': targetUser.first_name, 'lastName': targetUser.last_name }
             
             return JsonResponse({ "msg": "Sign-in was successful.", 'user': user })
         else:
@@ -44,10 +65,11 @@ def signIn(request):
 def createUser(request):
     print("request: ", request)
 
-    if request.POST and (request.POST.get('name') == 'createUser'):
+    if request.POST and (request.POST.get('name') == 'createUser' and request.POST.get('email')):
+        print("creating user")
         email = request.POST.get('email')
 
-        if User.objects.get(email=email):
+        if len(User.objects.filter(email=email)) == 1:
             print("The email was taken already.")
             return HttpResponse("There is an account with that email already.", status=409)
 
@@ -59,14 +81,11 @@ def createUser(request):
         fromCountry = request.POST.get('fromCountry')
         sex = request.POST.get('sex')
         phoneNum = request.POST.get('phoneNum')
-        isAllUserInfoPresent = (
-            email and password and firstName and lastName and birthDate and phoneNum and fromCity and fromCountry and sex)
+        isAllUserInfoPresent = (email and password and firstName and lastName and birthDate and phoneNum and fromCity and fromCountry and sex)
 
         if isAllUserInfoPresent:
             try:
-                user = User.objects.create_user(
-                    email=email, date_of_birth=birthDate, password=password)
-                print("user after creation: ", user)
+                User.objects.create_user(email=email, date_of_birth=birthDate, password=password)
                 newUser = User.objects.get(email=email)
                 newUser.first_name = firstName
                 newUser.last_name = lastName
@@ -75,13 +94,21 @@ def createUser(request):
                 newUser.from_country = fromCountry
                 newUser.sex = sex
                 newUser.save()
+                serverUrl = "{serverDomain}/users/get-token".format(serverDomain=serverDomain)
+                jwtResponse = getJwtResponse(serverUrl, email, password)
 
-                return HttpResponse("User was created successfully.")
+                return JsonResponse({"msg": "User was successfully created.", "jwtToken": jwtResponse.json() })
             except Exception as error:
-                print(
-                    "An error occurred while saving the user into the database: ", error)
+                print("An error occurred while saving the user into the database: ", error)
                 return HttpResponse("User info may not have been saved. Have client refresh the page.", status=404)
         else:
             return HttpResponse("Did not receive an email, password, first name, or a last name.", status=404)
 
     return HttpResponse("Request does not have a name.")
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getAccountInfo(request):
+    print("request, getting user account info: ", request)
+
+    return HttpResponse("User info attained.", status=200)
