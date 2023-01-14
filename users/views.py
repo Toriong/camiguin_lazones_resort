f7114a45891209b90e2164880ab363e4156c4415
Gabe <gtorion97@gmail.com>
2023-01-14 13:40:10 -0600

    getting jwt token

--- users/views.py
+++ users/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate, login
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 
-# Create your views here.
+
+def generateToken(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {'refresh': str(refresh), 'access': str(refresh.access_token)}
+
+
+# Views below.
 
 
 def testConnection(request):
@@ -16,21 +24,23 @@ def signIn(request):
     print("request: ", request)
 
     if request.GET and (request.GET.get('email') and request.GET.get('password')):
-        user = authenticate(request, email=request.GET.get('email'), password=request.GET.get('password'))
-
-        print("user: ", user)
+        email = request.GET.get('email')
+        user = authenticate(request, email=email, password=request.GET.get('password'))
 
         if user is not None:
-            print("user: ", user)
-            login(request, user)
+            targetUser = User.objects.get(email=email)
+            user = { "token": generateToken(user), 'firstName': targetUser.first_name, 'lastName': targetUser.last_name }
+            
+            return JsonResponse({ "msg": "Sign-in was successful.", 'user': user })
         else:
-            print("")
+            print("Invalid username or password.")
 
+            return HttpResponse("Invalid username or password.", status=404)
 
     return HttpResponse("Invalid parameters.", status=404)
-        
 
-@csrf_exempt 
+
+@csrf_exempt
 def createUser(request):
     print("request: ", request)
 
@@ -49,11 +59,13 @@ def createUser(request):
         fromCountry = request.POST.get('fromCountry')
         sex = request.POST.get('sex')
         phoneNum = request.POST.get('phoneNum')
-        isAllUserInfoPresent = (email and password and firstName and lastName and birthDate and phoneNum and fromCity and fromCountry and sex)
+        isAllUserInfoPresent = (
+            email and password and firstName and lastName and birthDate and phoneNum and fromCity and fromCountry and sex)
 
         if isAllUserInfoPresent:
             try:
-                user = User.objects.create_user(email=email, date_of_birth=birthDate, password=password) 
+                user = User.objects.create_user(
+                    email=email, date_of_birth=birthDate, password=password)
                 print("user after creation: ", user)
                 newUser = User.objects.get(email=email)
                 newUser.first_name = firstName
@@ -66,11 +78,10 @@ def createUser(request):
 
                 return HttpResponse("User was created successfully.")
             except Exception as error:
-                print("An error occurred while saving the user into the database: ", error)
+                print(
+                    "An error occurred while saving the user into the database: ", error)
                 return HttpResponse("User info may not have been saved. Have client refresh the page.", status=404)
         else:
             return HttpResponse("Did not receive an email, password, first name, or a last name.", status=404)
-        
-
 
     return HttpResponse("Request does not have a name.")
