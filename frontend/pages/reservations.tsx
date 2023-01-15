33e0fe7597631f6b56b057db09fe2c9175a28c65
Gabe <gtorion97@gmail.com>
2023-01-14 19:37:48 -0600

    creating the reservation page,wip

---
+++ frontend/pages/reservations.tsx
@@ -0,0 +1,14 @@
+
+
+function reservation(){
+    return (
+        <div className='reservationsPg'>
+            {/* create the reservation page nav which will consist of the following: */}
+            {/* reserve section: this will have the input */}
+            {/* find reservation: this will have the a look up for past and current reservation */}
+        </div>
+    )
+}
+
+
+export default reservation;
